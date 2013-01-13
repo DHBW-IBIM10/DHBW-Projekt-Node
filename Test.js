20eb5b89b9235ca0e63daa3d2707fac2628dd65f
mv <registration@max-v.de>
2013-01-13 18:34:20 +0100

    test server for evaluation purposes

--- Test.js
+++ Test.js
@@ -3,33 +3,93 @@ var express = require("express");
 var app = express();
 
 
+
 app.configure(function(){
     // middleware to parse JSON request bodies
     app.use(express.bodyParser());
 });
 
 // catch the request for xyz
-app.post('/versicherung1/val_kontakt', function (req, res) {
-    // surname
+app.post('/insurances', function (req, res) {
 
-    // firstname
+                  console.log("yes");
+    console.log(req.body);
+    if(req.body == "") {
+        console.log("empty");
+    }
 
-    // birthday
+    for(var i = 0; i < req.body.length; i++) {
+        console.log(req.body[i].name);
+    }
 
-    // street and no
+    console.log(req.body.toString());
 
-    // zip code
+    var valid = true;
 
-    // city
+//    // address
+//    if(!(req.body.anredeInput).match(/(Mr.|Mrs.|Ms.)?/i)) {
+//        valid = false;
+//    }
+//
+//    // title
+//    if(!(req.body.anredeInput).match(/(Sir|Dipl.|Dr.|Prof. Dr.|Dr. h.c.|)?/i)) {
+//        valid = false;
+//    }
+//
+//    // surname
+//    if(!(req.body.surname).match(/^[A-ZÄÖÜ][a-zäöüA-ZÄÖÜ\-' ]*[a-zäöüA-ZÄÖÜ ]$/i)) {
+//        valid = false;
+//    }
+//    // firstname
+//    if(!(req.body.firstname).match(/^([^ \x21-\x26\x28-\x2C\x2E-\x40\x5B-\x60\x7B-\xAC\xAE-\xBF\xF7\xFE]+)$/i)) {
+//        valid = false;
+//    }
+//
+//    // birthday
+//    if(!(req.body.birthday).match(/^([0-9]{0,2}).([0-9]{0,2}).([0-9]{0,4})$/)) {
+//        valid = false;
+//    }
+//
+//    // address
+//    if(!(req.body.maritalStatus).match(/(single|married|divorced|widowed)?/i)) {
+//        valid = false;
+//    }
+//
+//    // street and no
+//    if(!(req.body.street).match(/^(([a-zA-ZäöüÄÖÜ]\D*)\s+\d+?\s*.*)$/i)) {
+//        valid = false;
+//    }
+//
+//    // zip code
+//    if(!(req.body.street).match(/^\d{5}$|^[0-9]{5}(-[0-9]{4})?$|^([A-PR-UWYZ]([0-9]{1,2}|([A-HK-Y][0-9]|[A-HK-Y][0-9]([0-9]|[ABEHMNPRV-Y]))|[0-9][A-HJKS-UW])\ [0-9][ABD-HJLNP-UW-Z]{2}|(GIR\ 0AA)|(SAN\ TA1)|(BFPO\ (C\/O\ )?[0-9]{1,4})|((ASCN|BBND|[BFS]IQQ|PCRN|STHL|TDCU|TKCA)\ 1ZZ))$/)) {
+//        valid = false;
+//    }
+//
+//    // city
+//    if(!(req.body.street).match(/^[A-ZAÖÜ][a-zäöüA-ZÄÖÜ\-' ]*[a-zäöüA-ZÄÖÜ ]$/i)) {
+//        valid = false;
+//    }
+//
+//    // employee group
+//    if(!(req.body.surname).match(/^[A-ZAÖÜ][a-zaöüA-ZAÖÜ\-' ]*[a-zaöüA-ZAÖÜ ]$/i)) {
+//        valid = false;
+//    }
+//
+//    // amount insured
+//    if(!(req.body.amountInsured).match(/^\€?([1-9]{1}[0-9]{0,2}(\.[0-9]{3})*(\,[0-9]{0,2})?|[1-9]{1}[0-9]{0,}(\,[0-9]{0,2})?|0(\,[0-9]{0,2})?|(\,[0-9]{1,2})?)$/i)) {
+//        valid = false;
+//    }
+//
+//    // cost sharing
+//    if(!(req.body.ownershipStake).match(/^\€?([1-9]{1}[0-9]{0,2}(\.[0-9]{3})*(\,[0-9]{0,2})?|[1-9]{1}[0-9]{0,}(\,[0-9]{0,2})?|0(\,[0-9]{0,2})?|(\,[0-9]{1,2})?)$/)) {
+//        valid = false;
+//    }
 
-    // amount insured
-
-    // cost sharing
     res.contentType("text/html");
-    //res.send(200, "Hallo Welt!");
-    res.send(200, req.body.surname)
+    res.send(200, "Hallo Welt!");
+    //res.send(200, req.body.surname)
 
 });
 
 // Create the http server on port 3000
-app.listen(3000);
+app.listen(8888);
--- server.js
+++ server.js
@@ -7,6 +7,28 @@ var http = require("http");
 
 http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
+
+
+    var arr = [
+        {"name":"titel","value":"Dipl."}
+//        {"name":"vorname","value":"Max"},
+//        {"name":"nachname","value":"Mustermann"},
+//        {"name":"geburtstag","value":"12.02.2012"},
+//        {"name":"stand","value":"ledig"},
+//        {"name":"straßeNummer","value":"Musterstraße 1"},
+//        {"name":"plz","value":"70199"},
+//        {"name":"stadt","value":"Musterhausen"},
+//        {"name":"beruf","value":"Mustermann"},
+//        {"name":"summe","value":"5000000"},
+//        {"name":"behalt","value":"50000"}
+    ];
+
+    //for(var i = 0; i < arr.length; i++) {
+    var output = arr[0].name;
+
+    //}
+    response.write(output);
+
     response.end();
+
 }).listen(8888);
