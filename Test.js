284f8bc3f0f3af604f0185ef3f832c8eca03fd03
mv <registration@max-v.de>
2013-01-13 20:24:19 +0100

    server side validation works now.

--- Test.js
+++ Test.js
@@ -2,6 +2,8 @@
 var express = require("express");
 var app = express();
 
+// enabling Express to service static files. use /UI as root for localhost
+app.use(express.static(__dirname + '/UI'));
 
 
 app.configure(function(){
@@ -12,82 +14,94 @@ app.configure(function(){
 // catch the request for xyz
 app.post('/insurances', function (req, res) {
 
-                  console.log("yes");
-    console.log(req.body);
-    if(req.body == "") {
-        console.log("empty");
-    }
+    console.log("Request received.");
+
+    // set validation variable true and set it false when one of the validations fails
+    var valid = true;
 
     for(var i = 0; i < req.body.length; i++) {
         console.log(req.body[i].name);
+
+        switch(req.body[i].name) {
+            case 'address':
+                if(!(req.body[i].value).match(/(Mr.|Mrs.|Ms.)?/i)) {
+                    valid = false;
+                    break;
+                }
+                break;
+            case 'title':
+                if(!(req.body[i].value).match(/(Sir|Dipl.|Dr.|Prof. Dr.|Dr. h.c.|)?/i)) {
+                    valid = false;
+                    break;
+                }
+                break;
+            case 'surname':
+                if(!(req.body[i].value).match(/^[A-ZÄÖÜ][a-zäöüA-ZÄÖÜ\-' ]*[a-zäöüA-ZÄÖÜ ]$/i)) {
+                    valid = false;
+                    break;
+                }
+                break;
+            case 'name':
+                if(!(req.body[i].value).match(/^([^ \x21-\x26\x28-\x2C\x2E-\x40\x5B-\x60\x7B-\xAC\xAE-\xBF\xF7\xFE]+)$/i)) {
+                    valid = false;
+                    break;
+                }
+                break;
+            case 'birthday':
+                if(!(req.body[i].value).match(/^([0-9]{0,2}).([0-9]{0,2}).([0-9]{0,4})$/)) {
+                    valid = false;
+                    break;
+                }
+                break;
+            case 'maritalStatus':
+                if(!(req.body[i].value).match(/(single|married|divorced|widowed)?/i)) {
+                    valid = false;
+                    break;
+                }
+                break;
+            case 'streetNr':
+                if(!(req.body[i].value).match(/^(([a-zA-ZäöüÄÖÜ]\D*)\s+\d+?\s*.*)$/i)) {
+                    valid = false;
+                    break;
+                }
+                break;
+            case 'zip':
+                if(!(req.body[i].value).match(/^\d{5}$|^[0-9]{5}(-[0-9]{4})?$|^([A-PR-UWYZ]([0-9]{1,2}|([A-HK-Y][0-9]|[A-HK-Y][0-9]([0-9]|[ABEHMNPRV-Y]))|[0-9][A-HJKS-UW])\ [0-9][ABD-HJLNP-UW-Z]{2}|(GIR\ 0AA)|(SAN\ TA1)|(BFPO\ (C\/O\ )?[0-9]{1,4})|((ASCN|BBND|[BFS]IQQ|PCRN|STHL|TDCU|TKCA)\ 1ZZ))$/)) {
+                    valid = false;
+                    break;
+                }
+                break;
+            case 'city':
+                if(!(req.body[i].value).match(/^[A-ZAÖÜ][a-zäöüA-ZÄÖÜ\-' ]*[a-zäöüA-ZÄÖÜ ]$/i)) {
+                    valid = false;
+                    break;
+                }
+                break;
+            case 'career':
+                if(!(req.body[i].value).match(/^[A-ZAÖÜ][a-zaöüA-ZAÖÜ\-' ]*[a-zaöüA-ZAÖÜ ]$/i)) {
+                    valid = false;
+                    break;
+                }
+                break;
+            case 'amountInsured':
+                if(!(req.body[i].value).match(/^\€?([1-9]{1}[0-9]{0,2}(\.[0-9]{3})*(\,[0-9]{0,2})?|[1-9]{1}[0-9]{0,}(\,[0-9]{0,2})?|0(\,[0-9]{0,2})?|(\,[0-9]{1,2})?)$/i)) {
+                    valid = false;
+                    break;
+                }
+                break;
+            case 'ownershipStake':
+                if(!(req.body[i].value).match(/^\€?([1-9]{1}[0-9]{0,2}(\.[0-9]{3})*(\,[0-9]{0,2})?|[1-9]{1}[0-9]{0,}(\,[0-9]{0,2})?|0(\,[0-9]{0,2})?|(\,[0-9]{1,2})?)$/)) {
+                    valid = false;
+                    break;
+                }
+                break;
+        }
     }
 
-    console.log(req.body.toString());
-
-    var valid = true;
-
-//    // address
-//    if(!(req.body.anredeInput).match(/(Mr.|Mrs.|Ms.)?/i)) {
-//        valid = false;
-//    }
-//
-//    // title
-//    if(!(req.body.anredeInput).match(/(Sir|Dipl.|Dr.|Prof. Dr.|Dr. h.c.|)?/i)) {
-//        valid = false;
-//    }
-//
-//    // surname
-//    if(!(req.body.surname).match(/^[A-ZÄÖÜ][a-zäöüA-ZÄÖÜ\-' ]*[a-zäöüA-ZÄÖÜ ]$/i)) {
-//        valid = false;
-//    }
-//    // firstname
-//    if(!(req.body.firstname).match(/^([^ \x21-\x26\x28-\x2C\x2E-\x40\x5B-\x60\x7B-\xAC\xAE-\xBF\xF7\xFE]+)$/i)) {
-//        valid = false;
-//    }
-//
-//    // birthday
-//    if(!(req.body.birthday).match(/^([0-9]{0,2}).([0-9]{0,2}).([0-9]{0,4})$/)) {
-//        valid = false;
-//    }
-//
-//    // address
-//    if(!(req.body.maritalStatus).match(/(single|married|divorced|widowed)?/i)) {
-//        valid = false;
-//    }
-//
-//    // street and no
-//    if(!(req.body.street).match(/^(([a-zA-ZäöüÄÖÜ]\D*)\s+\d+?\s*.*)$/i)) {
-//        valid = false;
-//    }
-//
-//    // zip code
-//    if(!(req.body.street).match(/^\d{5}$|^[0-9]{5}(-[0-9]{4})?$|^([A-PR-UWYZ]([0-9]{1,2}|([A-HK-Y][0-9]|[A-HK-Y][0-9]([0-9]|[ABEHMNPRV-Y]))|[0-9][A-HJKS-UW])\ [0-9][ABD-HJLNP-UW-Z]{2}|(GIR\ 0AA)|(SAN\ TA1)|(BFPO\ (C\/O\ )?[0-9]{1,4})|((ASCN|BBND|[BFS]IQQ|PCRN|STHL|TDCU|TKCA)\ 1ZZ))$/)) {
-//        valid = false;
-//    }
-//
-//    // city
-//    if(!(req.body.street).match(/^[A-ZAÖÜ][a-zäöüA-ZÄÖÜ\-' ]*[a-zäöüA-ZÄÖÜ ]$/i)) {
-//        valid = false;
-//    }
-//
-//    // employee group
-//    if(!(req.body.surname).match(/^[A-ZAÖÜ][a-zaöüA-ZAÖÜ\-' ]*[a-zaöüA-ZAÖÜ ]$/i)) {
-//        valid = false;
-//    }
-//
-//    // amount insured
-//    if(!(req.body.amountInsured).match(/^\€?([1-9]{1}[0-9]{0,2}(\.[0-9]{3})*(\,[0-9]{0,2})?|[1-9]{1}[0-9]{0,}(\,[0-9]{0,2})?|0(\,[0-9]{0,2})?|(\,[0-9]{1,2})?)$/i)) {
-//        valid = false;
-//    }
-//
-//    // cost sharing
-//    if(!(req.body.ownershipStake).match(/^\€?([1-9]{1}[0-9]{0,2}(\.[0-9]{3})*(\,[0-9]{0,2})?|[1-9]{1}[0-9]{0,}(\,[0-9]{0,2})?|0(\,[0-9]{0,2})?|(\,[0-9]{1,2})?)$/)) {
-//        valid = false;
-//    }
+    console.log("Validation result: " + (valid ? "all valid" : "not valid"));
 
     res.contentType("text/html");
-    res.send(200, "Hallo Welt!");
-    //res.send(200, req.body.surname)
+    res.send(200, "OK");
 
 });
 
