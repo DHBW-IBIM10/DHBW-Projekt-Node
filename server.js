e1d17969d9361085bea31bf5b6114efbc3b01cfe
mv <registration@max-v.de>
2013-01-19 23:43:32 +0100

    live forms

--- server.js
+++ server.js
@@ -10,6 +10,22 @@ server.listen(8888);
 
 app.use(express.static(__dirname + '/'));
 
+app.configure(function(){
+    // middleware to parse JSON request bodies
+    app.use(express.bodyParser());
+});
+
+app.post('/pricecalculation', function (req, res) {
+
+    var JSONdata = {price: Math.round(Math.random()*(req.body[0].SummeInput-req.body[0].BehaltInput))};
+    console.log(JSON.stringify(JSONdata));
+
+    res.contentType("application/json");
+    res.send(200, JSON.stringify(JSONdata));
+
+});
+
+
 io.sockets.on('connection', function (socket) {
 
     // create new form
